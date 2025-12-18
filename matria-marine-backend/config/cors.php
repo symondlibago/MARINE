341b0072ccb498b47f580689302fea3cfd4fbe5e
symondlibago <libago.symond1@gmail.com>
2025-12-18 11:15:34 +0800

    deployed

--- matria-marine-backend/config/cors.php
+++ matria-marine-backend/config/cors.php
@@ -8,7 +8,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
-        'marine-xi-seven.vercel.app',
+        'https://marine-xi-seven.vercel.app', 
     ],
 
     'allowed_origins_patterns' => [],
