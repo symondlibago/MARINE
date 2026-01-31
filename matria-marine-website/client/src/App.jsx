e8a510f66facb50307187d5d1e48756287d53407
symondlibago <libago.symond1@gmail.com>
2026-01-31 17:48:25 +0800

    analytics

--- matria-marine-website/client/src/App.jsx
+++ matria-marine-website/client/src/App.jsx
@@ -5,7 +5,7 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import NotFound from "@/pages/NotFound";
-
+import { Analytics } from '@vercel/analytics/react';
 function Router() {
   return (
     <Switch>
@@ -23,6 +23,7 @@ function App() {
         <TooltipProvider>
           <Toaster />
           <Router />
+          <Analytics /> 
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
